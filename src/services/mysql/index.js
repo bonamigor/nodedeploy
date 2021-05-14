714347553a69a40843c74fb7229995a1b8ad1a2a
Rafael Ribeiro Bonamigo <32879433+bonamigor@users.noreply.github.com>
2021-05-14 12:47:00 -0300

    Update index.js

--- src/services/mysql/index.js
+++ src/services/mysql/index.js
@@ -1,11 +1,6 @@
 const mysqlServer = require('mysql');
 
-const connection = mysqlServer.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-});
+const connection = mysqlServer.createConnection(process.env.JAWSDB_URL);
 
 const errorHandler = (error, msg, rejectFunction) => {
   console.error(error);
